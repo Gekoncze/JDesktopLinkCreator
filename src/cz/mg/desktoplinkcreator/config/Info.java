6642f3153b21e2305fcedfade1ae1716ba1cc5e6
Me <gekoncze@seznam.cz>
2020-08-04 17:55:10 +0200

    fixed label

--- src/cz/mg/desktoplinkcreator/config/Info.java
+++ src/cz/mg/desktoplinkcreator/config/Info.java
@@ -1,6 +1,6 @@
 package cz.mg.desktoplinkcreator.config;
 
 public class Info {
-    public static final String NAME = "Desktop File Creator";
+    public static final String NAME = "Desktop Link Creator";
     public static final String VERSION = "0.1.0";
 }
